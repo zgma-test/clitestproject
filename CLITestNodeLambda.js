3660f61dd75f8346d503c4f09e7ed48710abd1e4
zgma-test <uditharosha+zgma@gmail.com>
2020-06-12 15:00:21 +0530

    init

---
+++ CLITestNodeLambda.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const { isValid } = require('json-validity');
+
+const schema = {
+    name: 'string',
+    age: 'number',
+    gender: ['M', 'F']
+}
+
+exports.handler = async (event) => {
+
+    console.log(event);
+    let validEvent = isValid({ input: event, schema });
+    console.log("Is valid:", validEvent);
+
+    let notificationSent = false;
+    if (validEvent) {
+        try {
+            let data = await sns.publish({
+                Message: JSON.stringify(event),
+                Subject: "Validation Passed",
+                TopicArn: "arn:aws:sns:us-east-1:318300609668:MyNotificationTopic",
+                MessageStructure: "String",
+                MessageAttributes: {}
+            }).promise();
+            console.log("Published notification", data);
+            notificationSent = true;
+
+        } catch (err) {
+            console.log("Failed to publish notification", err);
+            notificationSent = false;
+        };
+
+    }
+    return { valid: validEvent, notification: notificationSent }
+};
